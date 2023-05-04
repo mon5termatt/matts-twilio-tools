e2d54ce133b9bca4c8d226128f1b9356baa8967b
Matthew Clark <43628254+mon5termatt@users.noreply.github.com>
2023-05-03 23:03:09 -0500

    Update never-gonna-give-you-up.js

--- never-gonna-give-you-up.js
+++ never-gonna-give-you-up.js
@@ -1,6 +1,6 @@
 exports.handler = function (context, event, callback) {
   const twiml = new Twilio.twiml.VoiceResponse();
-  twiml.play('https://demo.twilio.com/docs/classic.mp3');
+  twiml.play('https://github.com/mon5termatt/matts-twilio-tools/raw/main/river.mp3');
 
   callback(null, twiml);
 };
